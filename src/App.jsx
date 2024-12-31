c6dfa8190ca81aa1d7ae5368da2066bc54f07e19
jeshwanthshivasai <git@stackblitz.com>
2024-12-31 08:38:50 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// import logo from './logo.svg';
+import './App.css';
+import Mainpage from './Components/Main';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Mainpage />} />
+        <Route path="/loginpage" element={<Login />} />
+        <Route path="/signinpage" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
